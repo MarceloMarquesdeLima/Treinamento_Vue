bcf1d625daea81484d8b59632c0484e323616951
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2023-06-07 18:44:10 -0300

    Projeto Complementar Vue
    Projeto Complementar Vue treinamento

---
+++ src/components/Form.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <form action="">
+            <h1>Meu Formulário</h1>
+            <div>
+                <InputText />
+            </div> <br />
+            <div>
+                <InputText />
+            </div> <br />
+            <div>
+                <Submit />
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    import InputText from './form/inputText.vue'
+    import Submit from './form/Submit.vue'
+
+    export default {
+        name: 'Form',
+        components: {
+            InputText,
+            Submit
+        }
+    }
+</script>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Olá vue!</h1>
+      <p>Nome é: {{ nome }} e trabalho com {{ profissao }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: "Marcelo",
+                profissao: "Desenvolvedor"
+            }
+        }
+    }
+</script>
